9443a7b1456c73cdc9dfd21e69389118582f9737
BraineUA <bogdan.cys.centrum@gmail.com>
2018-01-31 22:04:29 +0300

    Add start project files.

---
+++ app/src/main/java/com/example/timteam/youwillwin/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.timteam.youwillwin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void clickFav(View view) {
+    }
+
+    public void clickCalendar(View view) {
+    }
+
+    public void clickMenu(View view) {
+    }
+}
