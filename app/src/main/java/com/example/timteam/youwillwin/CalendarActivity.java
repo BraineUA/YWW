cdd00d169d6f5fc23a0c894a887e8c8579c7fbc9
BraineUA <bogdan.cys.centrum@gmail.com>
2018-01-31 22:47:48 +0300

    Add calendar.

---
+++ app/src/main/java/com/example/timteam/youwillwin/CalendarActivity.java
@@ -0,0 +1,17 @@
+package com.example.timteam.youwillwin;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by TimTeam on 31.01.2018.
+ */
+
+public class CalendarActivity extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
