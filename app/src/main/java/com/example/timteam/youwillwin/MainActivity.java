cec4bb68f0fe95d8553a1a1fe35ec2a491441d8e
BraineUA <bogdan.cys.centrum@gmail.com>
2018-02-01 05:13:39 +0300

    Add drawable,logik mainActivity.

--- app/src/main/java/com/example/timteam/youwillwin/MainActivity.java
+++ app/src/main/java/com/example/timteam/youwillwin/MainActivity.java
@@ -1,34 +1,50 @@
 package com.example.timteam.youwillwin;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.like.LikeButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
     ImageView button_Calendar;
     ImageView button_favorites;
     ImageView button_menu;
-    ImageView status;
+    ImageView imageView;
+    LikeButton likeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button_Calendar =findViewById(R.id.button_calendar);
-        button_favorites=findViewById(R.id.button_favorites);
-        button_menu=findViewById(R.id.button_menu);
-        status = findViewById(R.id.status);
+        button_Calendar = findViewById(R.id.button_calendar);
+        button_favorites = findViewById(R.id.button_favorites);
+        button_menu = findViewById(R.id.button_menu);
+        imageView = findViewById(R.id.imageView);
+
+        String month = new SimpleDateFormat("MMMM", Locale.ENGLISH).format(new Date());
+        String day = new SimpleDateFormat("d", Locale.ENGLISH).format(new Date());
+        String fileName = month.toLowerCase() + "_" + day;
+        Context context = imageView.getContext();
+        int id = context.getResources().getIdentifier(fileName, "drawable", context.getPackageName());
+        imageView.setImageResource(id);
+
+
     }
-
-
 
     public void clickFav(View view) {
     }
 
     public void clickCalendar(View view) {
-        Intent intent_calendar = new Intent(this,CalendarActivity.class);
+        Intent intent_calendar = new Intent(this, CalendarActivity.class);
         startActivity(intent_calendar);
 
 
